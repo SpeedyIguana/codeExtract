456efb2792ffbfe327249ef0e254e6f4c32f1507
akassh <akassh.shah77@gmail.com>
2020-05-09 03:11:00 -0400

    final for 3:10 AM

--- main.py
+++ main.py
@@ -72,9 +72,9 @@ if __name__ == '__main__':
             if requests.get(parentUrl).ok and requests.get(commitUrl).ok:
                 fn = functionCalls.get(parentDomain, None)
                 if fn is None:
-                    print("*" * 9)
+                    print("*" * 20)
                     print("Missing Dict Entry for i = ", i, ", domain = ", parentDomain)
-                    print("*" * 9)
+                    print("*" * 20)
                     continue
                 print("-------------------------------------")
                 print(i, parentUrl, commitUrl)
