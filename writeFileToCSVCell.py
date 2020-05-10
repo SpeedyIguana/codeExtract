ff9032fa7ac9e93b7c2a0abac26e111492063bfe
akassh <akassh.shah77@gmail.com>
2020-05-10 03:04:04 -0400

    added script to write data from file to one cell

---
+++ writeFileToCSVCell.py
@@ -0,0 +1,15 @@
+import json
+import pandas as pd
+
+with open('writeFileToCSVCellSettings.json', 'r') as infile:
+    settings = json.load(infile)
+
+data = pd.read_csv(settings["csvFrom"])
+
+with open(settings["fileFrom"], 'r') as infile:
+    txt = infile.read()
+
+if __name__ == "__main__":
+    data.iloc[int(settings["rowToInserInto"]), data.columns.get_loc(settings["columnNameTo"])] = txt
+    print(data.iloc[int(settings["rowToInserInto"]), data.columns.get_loc(settings["columnNameTo"])])
+    data.to_csv(settings["csvTo"], index=False)
